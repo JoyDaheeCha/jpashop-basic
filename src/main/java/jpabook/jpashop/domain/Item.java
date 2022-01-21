979e2b2d8d38b63813c97470f2d70da9fc9e9c6e
JoyDaheeCha <daheecha0612@gmail.com>
2022-01-22 03:34:08 +0900

    다:다 예시 (실무에서는 1:N, N:1로 반드시 풀어내자)

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -3,6 +3,9 @@ package jpabook.jpashop.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Item {
@@ -12,10 +15,11 @@ public class Item {
     private Long id;
 
     private String name;
-
     private int price;
-
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
     public Long getId() {
         return id;
